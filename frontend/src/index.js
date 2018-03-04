5bf001faa5794bbf8ccc88ff4155b5e759620856
ggibsen <ggibsen@users.noreply.github.com>
2018-03-03 23:57:48 -0800

    using diff versions of helloworld component (w/ and w/out props passed)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-// import HelloWorld from './App';
+// import App from './App';
+import HelloWorld from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<HelloWorld title="Testgg1"/>, document.getElementById('rootNew'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HelloWorld title="Testgg2"/>, document.getElementById('rootNew'));
+// ReactDOM.render(<HelloWorld />, document.getElementById('rootNew'));
 
 registerServiceWorker();
