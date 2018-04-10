4e615ef7d2c6f48372006c09d9ac454e0acdc6e5
ggibsen <ggibsen@users.noreply.github.com>
2018-04-09 23:39:08 -0700

    building first GCP clound function

---
+++ funcs/rand.js
@@ -0,0 +1,21 @@
+/**
+ * Returns randomized number to any HTTP request, optionally using a "playerContext" field in the body
+ * to help calculate the randomized number.
+ *
+ * @param {!Object} req Cloud Function request context.
+ * @param {!Object} res Cloud Function response context.
+ */
+exports.random = (req, res) => {
+    // Example input: {"playerContext": 3}
+    if (req.body.playerContext !== undefined) {
+        //TODO use playerContext
+        console.log(`got playerContext: ${req.body.playerContext}`);
+        // res.status(400).send('No playerContext defined!');
+    }
+    else {
+        // player context is optional, so just ignore
+        var randomNum = 10 * Math.random();
+        console.log(`generated: ${randomNum}`);
+        res.status(200).send(randomNum);
+    }
+};
